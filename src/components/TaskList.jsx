54e99129fd4001725ee415a200f095c1bc7e12b7
kulotsystems <kulotsystems@gmail.com>
2023-02-03 19:01:51 +0800

    Setup Supabase.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from 'react';
 import { Box, HStack, Image, StackDivider, Text, VStack } from '@chakra-ui/react';
 import ClearTasks from './ClearTasks.jsx';
 import DeleteTask from './DeleteTask.jsx';
 import img from '../assets/empty.svg';
+import supabase from '../supabase.js';
 
 const TaskList = () => {
+    const [tasks, setTasks] = useState([]);
+
+    // onload
+    useEffect(() => {
+        // fetch data
+        const fetchData = async () => {
+            let { data: tasks, error } = await supabase
+                .from('todos')
+                .select('*');
+
+            if(error) {
+
+            }
+
+            if(tasks) {
+                setTasks(tasks);
+            }
+        };
+
+        fetchData().then(response => {});
+    }, []);
+
+
     // If there are no tasks, show placeholder image
-    return (
-        <Box align="center">
-            <Image src={img} mt="30px" maxW="95%"/>
-        </Box>
-    );
+    if(tasks.length <= 0) {
+        return (
+            <Box align="center">
+                <Image src={ img } mt="30px" maxW="95%"/>
+            </Box>
+        );
+    }
 
     return (
         <>
@@ -23,12 +50,14 @@ const TaskList = () => {
                 maxW={{ base: '90vw', sm: '80vw', lg: '500vw', xl: '30vw'}}
                 alignItems="stretch"
             >
-                {/*<HStack key="">*/}
-                {/*    <Text w="100%" p="8px" borderRadius="lg">*/}
-                {/*        Wash the dishes*/}
-                {/*    </Text>*/}
-                {/*    <DeleteTask/>*/}
-                {/*</HStack>*/}
+                {tasks.map(task => (
+                    <HStack key={task.id}>
+                        <Text w="100%" p="8px" borderRadius="lg">
+                            {task.text}
+                        </Text>
+                        <DeleteTask/>
+                    </HStack>
+                ))}
             </VStack>
 
             <ClearTasks/>
